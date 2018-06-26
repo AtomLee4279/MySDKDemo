3d2eb9f0f70c96d2bb57cac24871c5bfd5123b4e
李一贤 <573096385@qq.com>
2018-06-26 10:27:24 +0800

    sdk初步提交

---
+++ MySDKDemoKit/MySDKDemoKit.h
@@ -0,0 +1,19 @@
+//
+//  MySDKDemoKit.h
+//  MySDKDemoKit
+//
+//  Created by 李一贤 on 2018/6/26.
+//  Copyright © 2018年 李一贤. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MySDKDemoKit.
+FOUNDATION_EXPORT double MySDKDemoKitVersionNumber;
+
+//! Project version string for MySDKDemoKit.
+FOUNDATION_EXPORT const unsigned char MySDKDemoKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MySDKDemoKit/PublicHeader.h>
+
+
